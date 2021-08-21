b7294d82d0b23f39d6669e0ee1e484a7e0db5bc9
ducng99 <tom@ducng.dev>
2021-08-22 04:18:23 +1200

    Add project files.

---
+++ XSync/App.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace XSync
+{
+    class App
+    {
+        private readonly List<string> FoldersPaths;
+
+        public App(List<string> paths)
+        {
+            FoldersPaths = paths;
+        }
+
+        public void Sync()
+        {
+            for (int i = 1; i < FoldersPaths.Count; i++)
+            {
+                Sync2Folders(FoldersPaths[0], FoldersPaths[i]);
+            }
+        }
+
+        private static void Sync2Folders(string folder1, string folder2)
+        {
+            Dictionary<string, (DateTime, byte)> LatestFiles = new();
+
+            var filesFolder1 = Directory.EnumerateFiles(folder1, "*", SearchOption.AllDirectories);
+            
+            foreach (string file in filesFolder1)
+            {
+                string relaPath = Path.GetRelativePath(folder1, file);
+                DateTime modifiedTime = File.GetLastWriteTime(file);
+                LatestFiles.Add(relaPath, (modifiedTime, 0));
+            }
+
+            var filesFolder2 = Directory.EnumerateFiles(folder2, "*", SearchOption.AllDirectories);
+
+            foreach (string file in filesFolder2)
+            {
+                string relaPath = Path.GetRelativePath(folder2, file);
+                DateTime modifiedTime = File.GetLastWriteTime(file);
+
+                if (!LatestFiles.ContainsKey(relaPath) || DateTime.Compare(LatestFiles[relaPath].Item1, modifiedTime) < 0)
+                {
+                    LatestFiles[relaPath] = (modifiedTime, 1);
+                }
+            }
+
+            foreach ((var relaPath, (var date, var index)) in LatestFiles)
+            {
+                if (index == 0)
+                {
+                    var destFile = Path.Combine(folder2, relaPath);
+                    new FileInfo(destFile).Directory.Create();
+
+                    File.Copy(Path.Combine(folder1, relaPath), destFile, true);
+                }
+                else if (index == 1)
+                {
+                    var destFile = Path.Combine(folder1, relaPath);
+                    new FileInfo(destFile).Directory.Create();
+
+                    File.Copy(Path.Combine(folder2, relaPath), destFile, true);
+                }
+            }
+        }
+    }
+}
---
+++ XSync/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace XSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                List<string> FolderPaths = new();
+
+                foreach (string arg in args)
+                {
+                    if (Directory.Exists(arg))
+                    {
+                        FolderPaths.Add(Path.GetFullPath(arg));
+                    }
+                }
+
+                var App = new App(FolderPaths);
+                App.Sync();
+            }
+            else
+            {
+                Console.WriteLine("Please supply 2 folders or more to sync");
+                Console.WriteLine();
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
